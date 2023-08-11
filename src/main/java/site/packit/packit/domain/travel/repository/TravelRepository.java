f98e7abde993539fa8ba44cfcca98281639b2aac
ziiyouth <ziy913@naver.com>
2023-08-11 21:11:26 +0900

    feat: #10 Travel Entity Class 생성

---
+++ src/main/java/site/packit/packit/domain/travel/repository/TravelRepository.java
@@ -0,0 +1,8 @@
+package site.packit.packit.domain.travel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import site.packit.packit.domain.travel.entity.Travel;
+
+public interface TravelRepository extends JpaRepository<Travel, Long> {
+
+}
