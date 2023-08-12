149f5174c6f3dd45dc8b52472614b3cc3d4c4eda
ziiyouth <ziy913@naver.com>
2023-08-12 22:38:41 +0900

    feat: 여행 도메인 기본 API 개발

--- src/main/java/site/packit/packit/domain/travel/repository/TravelRepository.java
+++ src/main/java/site/packit/packit/domain/travel/repository/TravelRepository.java
@@ -3,6 +3,7 @@ package site.packit.packit.domain.travel.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.packit.packit.domain.travel.entity.Travel;
 
-public interface TravelRepository extends JpaRepository<Travel, Long> {
+public interface TravelRepository
+        extends JpaRepository<Travel, Long> {
 
 }
--- src/main/java/site/packit/packit/domain/travel/service/TravelService.java
+++ src/main/java/site/packit/packit/domain/travel/service/TravelService.java
@@ -1,4 +1,86 @@
 package site.packit.packit.domain.travel.service;
 
-public interface TravelService {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.packit.packit.domain.member.entity.Member;
+import site.packit.packit.domain.member.repository.MemberRepository;
+import site.packit.packit.domain.travel.dto.CreateTravelRequest;
+import site.packit.packit.domain.travel.dto.UpdateTravelRequest;
+import site.packit.packit.domain.travel.entity.Travel;
+import site.packit.packit.domain.travel.repository.TravelRepository;
+import site.packit.packit.global.exception.ResourceNotFoundException;
+import static site.packit.packit.domain.member.exception.MemberErrorCode.MEMBER_NOT_FOUND;
+import static site.packit.packit.domain.travel.constant.TravelStatus.INTENDED;
+import static site.packit.packit.domain.travel.exception.TravelErrorCode.TRAVEL_NOT_FOUND;
+
+@Service
+public class TravelService {
+
+    private final MemberRepository memberRepository;
+    private final TravelRepository travelRepository;
+
+
+    public TravelService(MemberRepository memberRepository, TravelRepository travelRepository) {
+        this.memberRepository = memberRepository;
+        this.travelRepository = travelRepository;
+    }
+
+
+    /**
+     * 새로운 여행 생성
+     */
+    public Long createTravel(CreateTravelRequest createTravelRequest) {
+
+        if(!memberRepository.existsById(createTravelRequest.memberId())){
+            throw new ResourceNotFoundException(MEMBER_NOT_FOUND);
+        }
+
+        Member member = memberRepository.findById(createTravelRequest.memberId()).get();
+
+        Travel createTravel = Travel.builder()
+                .title(createTravelRequest.title())
+                .destinationType(createTravelRequest.destinationType())
+                .startDate(createTravelRequest.startDate())
+                .endDate(createTravelRequest.endDate())
+                .travelStatus(INTENDED)
+                .member(member)
+                .build();
+
+        travelRepository.save(createTravel);
+        return createTravel.getId();
+    }
+
+    /**
+     * 여행 수정
+     */
+    @Transactional
+    public void updateTravel(Long travelId, UpdateTravelRequest updateTravelRequest){
+
+//        Travel travel = travelRepository.findById(travelId).get()
+//                .orElseThrow(() -> new ResourceNotFoundException(TRAVEL_NOT_FOUND));
+        if(!travelRepository.existsById(travelId)){
+            throw new ResourceNotFoundException(TRAVEL_NOT_FOUND);
+        }
+        Travel travel = travelRepository.findById(travelId).get();
+        travel.updateTravel(updateTravelRequest.title(), updateTravelRequest.startDate(), updateTravelRequest.endDate());
+    }
+
+    /**
+     * 여행 삭제
+     */
+    @Transactional
+    public void deleteTravel(Long travelId){
+
+        if(!travelRepository.existsById(travelId)){
+            throw new ResourceNotFoundException(TRAVEL_NOT_FOUND);
+        }
+
+        // TODO : 여행 하위 항목 (리스트, 리스트 세부) 삭제 코드 추가
+
+        Travel travel = travelRepository.findById(travelId).get();
+        travelRepository.delete(travel);
+    }
+
+
+
 }
