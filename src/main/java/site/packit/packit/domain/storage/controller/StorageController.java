c766777b4f7c263a85aa6fbe3238af9e2aad92e1
ziiyouth <ziy913@naver.com>
2023-08-15 02:09:02 +0900

    feat : 보관함 Entity 생성

---
+++ src/main/java/site/packit/packit/domain/storage/controller/StorageController.java
@@ -0,0 +1,10 @@
+package site.packit.packit.domain.storage.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class StorageController {
+
+}
