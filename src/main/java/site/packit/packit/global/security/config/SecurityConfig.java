111574f6c039f81daa01a28d29f3090a40ae8d78
edgar6bf <edgar6bf@gmail.com>
2023-09-18 20:41:00 +0900

    style: #78 SecurityConfig 불필요한 스타일 제거

--- src/main/java/site/packit/packit/global/security/config/SecurityConfig.java
+++ src/main/java/site/packit/packit/global/security/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package site.packit.packit.global.security.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
